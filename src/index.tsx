4dd197d07afb9d5f89bc8551a13590b3e4ec04af
Snopok Stanislav <snopok.stanislav@mail.ru>
2021-03-20 17:40:11 +0300

    feat: add redux

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 reportWebVitals();
