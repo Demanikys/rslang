e8fd0d47f8c730019d74107872b46e509ef4b4c0
Bogdan <big_bogdan@rambler.ru>
2021-03-23 22:40:36 +0300

    feat: added basic functionality for score

--- src/pages/Sprint/Sprint.jsx
+++ src/pages/Sprint/Sprint.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import style from './sprint.module.scss';
 
 const Sprint = () => {
-  const a = 5;
+  const [points, setPoints] = useState(0);
+  const [level, setLevel] = useState([0, 0, 0]);
+  useEffect(() => {
+    const arr = [];
+    level.forEach((elem, index) => {
+      arr.push((index + 1 <= points) + 0);
+      console.log(index, arr, points);
+      setLevel(arr);
+    });
+  }, [points]);
   return (
-    <div>
-      Sprint is
-      {a}
+    <div className={style.wrapper}>
+      <div className={style.pointsNumber}>
+        0
+        <div className={style.gameWindow}>
+          {level}
+          <div className={style.points}>
+            <button
+              type="button"
+              onClick={() => {
+                if (points + 1 <= level.length) {
+                  setPoints(points + 1);
+                }
+              }}
+            >
+              add point
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setLevel(new Array(level.length + 1).fill(0));
+                setPoints(0);
+                console.log(level, points);
+              }}
+            >
+              add level
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setLevel(new Array(3).fill(0));
+                setPoints(0);
+              }}
+            >
+              reset
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
